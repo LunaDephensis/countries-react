059f901ced8996f653e77932b31f5758690801c9
Luna Dephensis <engetsu96@gmail.com>
2023-06-05 18:01:22 +0200

    kiválasztott ország adatainak lekérése

--- countries-react/src/App.jsx
+++ countries-react/src/App.jsx
@@ -1,17 +1,75 @@
 import { useState } from 'react'
 import Navigation from './components/Navigation'
 import CountryList from './components/CountryList'
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   let [isActiveDarkMode, setIsActiveDarkMode] = useState(false);
+  let [isShownDetails, setIsShownDetails] = useState(false);
 
   function toggleDarkMode() {
     setIsActiveDarkMode(prev => !prev);
   }
 
+  function toggleCountryDetails() {
+    setIsShownDetails(prev => !prev);
+  }
+
+  async function getCountryDetails(name) {
+    let res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+    let countryData = await res.json();
+    return countryData.map((element) => {
+      return {
+        name: element.name.common,
+        nativeName: getNativeNames(element.name.nativeName),
+        population: element.population,
+        region: element.region,
+        subregion: element.subregion,
+        capital: element.capital[0],
+        tld: element.tld[0],
+        currencies: getCurrencies(element.currencies),
+        languages: getLanguages(element.languages),
+        border: getBorders(element.borders)
+      }
+    });
+  }
+
+  function getNativeNames(natNameObj) {
+    let nativeNames = [];
+    for( let nativeName of natNameObj ) {
+      nativeNames.push(nativeName.common);
+    }
+    return nativeNames;
+  }
+
+  function getCurrencies(currObj) {
+    let currencies = [];
+    for ( let curr of currObj ) {
+      currencies.push(curr.name);
+    }
+    return currencies;
+  }
+
+  function getLanguages(langObj) {
+    let languages = [];
+    for ( let lang of langObj ) {
+      languages.push(lang);
+    }
+    return languages;
+  }
+
+   async function getBorders(borderCodes) {
+    let res = await fetch(`https://restcountries.com/v3.1/alpha?codes=${borderCodes.join()}&fields=name`);
+    let bordersData = await res.json();
+    return bordersData.map((border) => {
+      return border.name.common;
+    });
+  }
+
   return (
     <div className={`${isActiveDarkMode && "dark"} relative w-full min-h-screen flex justify-center items-center flex-col`}>
       <Navigation onToggleDarkMode={toggleDarkMode}/>
+      {isShownDetails ? <CountryDetails onToggleDetails={toggleCountryDetails}/> : <CountryList onToggleDetails={toggleCountryDetails}/>}
       <CountryList/>
     </div>
   )
--- countries-react/src/components/CountryDetails.jsx
+++ countries-react/src/components/CountryDetails.jsx
@@ -1,4 +1,6 @@
 function CountryDetails() {
+    let borderCountries = ["France", "Belgium", "Switzerland", "Denmark", "Poland", "Austria", "Luxembourg", "Netherlands"];
+
     return (
         <main className="relative w-full min-h-screen flex justify-start items-center flex-col bg-lightBg dark:bg-darkBg px-4 pt-28 sm:px-20">
             <button className="relative flex justify-center items-center border-none outline-none rounded py-2 px-6 text-lg font-normal shadow-lg bg-white dark:bg-darkMain text-darkText dark:text-white">
@@ -7,35 +9,36 @@ function CountryDetails() {
                 </span>
                 Back
             </button>
-            <section className="relative w-full flex justify-start items-center flex-col">
-                <img src="/flag.png" alt="national flag" />
-                <div>
-                    <h3>
+            <section className="relative w-full flex justify-start items-center flex-col px-6 sm:justify-between sm:flex-row sm:px-8">
+                <img className="relative w-full"
+                    src="/flag.png" alt="national flag"
+                />
+                <div className="relative w-full sm:w-[50%] flex justify-start items-start flex-col text-darkText dark:text-white">
+                    <h3 className="relative w-full text-lg font-bold mb-12">
                         Germany
                     </h3>
-                    <ul>
-                        <li>Native Name: <span>Asdasd</span></li>
-                        <li>Population: <span>123456123</span></li>
-                        <li>Region: <span>Europe</span></li>
-                        <li>Sub Region: <span>Western Europe</span></li>
-                        <li>Capital: <span>Berlin</span></li>
+                    <ul className="relative w-full mb-12">
+                        <li className="countryInfo">Native Name: <span>Asdasd</span></li>
+                        <li className="countryInfo">Population: <span>123456123</span></li>
+                        <li className="countryInfo">Region: <span>Europe</span></li>
+                        <li className="countryInfo">Sub Region: <span>Western Europe</span></li>
+                        <li className="countryInfo">Capital: <span>Berlin</span></li>
                     </ul>
-                    <ul>
-                        <li>Top Level Domain: <span>.de</span></li>
-                        <li>Currencies: <span>Euro</span></li>
-                        <li>Languages: <span>German</span></li>
+                    <ul className="relative w-full mb-12">
+                        <li className="countryInfo">Top Level Domain: <span>.de</span></li>
+                        <li className="countryInfo">Currencies: <span>Euro</span></li>
+                        <li className="countryInfo">Languages: <span>German</span></li>
                     </ul>
                     <div>
-                        <h4>Border Countries:</h4>
+                        <h4 className="tect-base font-bold">Border Countries:</h4>
                         <div>
-                            <span>France</span>
-                            <span>Poland</span>
-                            <span>Switzerland</span>
-                            <span>Austria</span>
-                            <span>Luxembourg</span>
-                            <span>Belgium</span>
-                            <span>Netherlands</span>
-                            <span>Denmark</span>
+                            {
+                                borderCountries.map((borderCountry, i) => {
+                                    return (<span key={i} className="relative flex justify-center items-center py-2 px-6 text-base font-light border-[0.5px] border-solid border-[rgba(0,0,0,0.1)] shadow-md rounded bg-white dark:bg-darkMain text-darkText dark:text-white">
+                                                {borderCountry}
+                                            </span>);
+                                })
+                            }
                         </div>
                     </div>
                 </div>
